c340a36b60dd1f1f59c85d3bc772961d1a06c60c
meganerisu <53895028+meganerisu@users.noreply.github.com>
2021-04-12 21:08:03 +0900

    13th 内容を追加

--- photoshare_flutter/lib/tree.dart
+++ photoshare_flutter/lib/tree.dart
@@ -17,21 +17,39 @@ class Album {
 // final List urlJimuDrive = ["www.microsoft.com"];
 // final List urlJimuGyoza = ["www.apple.com"];
 
-final Album jimuZushi = Album("jimuZushi", "");
-final Album jimuBBQ = Album("jimuBBQ", "");
-final Album jimuNomi = Album("jimuNomi", "");
-final Album jimuDrive = Album("jimuDrive", "www.googlephoto.com");
-final Album jimuSesshou = Album("kobetsuSesshou", "");
+final Album jimuZushi13 =
+    Album("jimuZushi13", "https://photos.app.goo.gl/knFwQkYYpHjU9dVd9");
+final Album jimuBBQ13 =
+    Album("jimuBBQ13", "https://photos.app.goo.gl/8U9oAVGSczLSSPWVA");
+// final Album jimuNomi13 = Album("jimuNomi13", "");
+final Album jimuDrive13 =
+    Album("jimuDrive13", "https://photos.app.goo.gl/zNX5kMgRozMWDR5e8");
+final Album jimuSesshou13 =
+    Album("kobetsuSesshou13", "https://photos.app.goo.gl/t7EVff13xwGqnnpC6");
 final Album jimuHakone =
-    Album("jimuHakone", "https://photos.app.goo.gl/GUaKcpuVhez61aLKA");
-final Album jimuGyoza =
-    Album("jimuGyoza", "https://photos.app.goo.gl/pCm7UXgAWqK53Y6e9");
-final Album jimuBowling = Album("jimuBowling", "");
+    Album("jimuHakone13", "https://photos.app.goo.gl/GUaKcpuVhez61aLKA");
+final Album jimuBowling = Album("jimuBowling13", "");
 final Album jimuChiba =
-    Album("jimuTrip", "https://photos.app.goo.gl/GUaKcpuVhez61aLKA");
+    Album("jimuTrip13", "https://photos.app.goo.gl/GUaKcpuVhez61aLKA");
+final Album jimuGyoza13 =
+    Album("jimuGyoza13", "https://photos.app.goo.gl/Nbh6ThtZthczmnvC9");
 
-Category jimu13th = Category("jimu13th", [jimuBBQ, jimuGyoza, jimuHakone]);
-Category jimu14th = Category("jimu14th", [jimuBBQ, jimuGyoza, jimuHakone]);
+final Album jimuBBQ14 = Album("jimuGyoza", "");
+
+final Album jimuGyoza15 =
+    Album("jimuGyoza", "https://photos.app.goo.gl/pCm7UXgAWqK53Y6e9");
+
+Category jimu13th = Category("jimu13th", [
+  jimuZushi13,
+  jimuBBQ13,
+  jimuDrive13,
+  jimuSesshou13,
+  jimuGyoza15,
+  jimuHakone,
+  jimuBowling,
+  jimuChiba
+]);
+Category jimu14th = Category("jimu14th", [jimuBBQ14, jimuGyoza15, jimuHakone]);
 Category jimu15th = Category("jimu15th", []);
 
 Category jimukyoku = Category("jimukyoku", [jimu13th, jimu14th, jimu15th]);
